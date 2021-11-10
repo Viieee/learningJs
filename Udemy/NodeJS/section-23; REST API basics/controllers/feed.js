abb1951632b18321db10bc73f51fec6b0b46f16e
Viieee <adhityaharviando@gmail.com>
2021-11-10 09:06:59 +0700

    start section 24

--- Udemy/NodeJS/section-23; REST API basics/controllers/feed.js
+++ Udemy/NodeJS/section-23; REST API basics/controllers/feed.js
@@ -1,6 +1,12 @@
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
-    posts: [{ title: "first post", content: "thit is my first post!" }],
+    posts: [
+      {
+        title: 'first post',
+        content: 'thit is my first post!',
+        imageUrl: 'images/2021-10-26-cursed02.jpg',
+      },
+    ],
   });
 };
 
@@ -9,7 +15,7 @@ exports.postPost = (req, res, next) => {
   const content = req.body.content;
   // crate post in db
   res.status(201).json({
-    message: "post created successfully",
+    message: 'post created successfully',
     post: {
       id: new Date().toISOString().slice(0, 10),
       title: title,
